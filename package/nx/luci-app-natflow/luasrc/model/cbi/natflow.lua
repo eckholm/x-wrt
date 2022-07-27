af0952cfca6e264a72cc3533020544408048605b
NxQAQVQ <wq4180056hey@163.com>
2022-07-27 09:18:09 +0800

    luci-app-natflow: fix cbi typo

--- package/nx/luci-app-natflow/luasrc/model/cbi/natflow.lua
+++ package/nx/luci-app-natflow/luasrc/model/cbi/natflow.lua
@@ -8,16 +8,16 @@ s.anonymous = true
 s:tab("system", translate("System Settings"))
 
 e = s:taboption("system", Flag, "fullconenat", translate("Full Cone Nat"), translate("Generally do not need to be enabled unless used to play games."))
-e.default = e.disabled
+e.default = "0"
 e.rmempty = false
 
 e = s:taboption("system", Flag, "enabled", translate("Enable Fast Forwarding"))
-e.default = e.disabled
+e.default = "1"
 e.rmempty = false
 
 e = s:taboption("system", Flag, "hwnat", translate("Enable Fast Forwarding Hardware Offload"))
-e.default = e.disabled
+e.default = "1"
 e.rmempty = false
-e:depends("enable","1")
+e:depends("enabled","1")
 
 return m
