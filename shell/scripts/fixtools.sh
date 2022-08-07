151aee8c5178db8b1e9746142cf75c6ce2b039ee
NxQAQVQ <wq4180056hey@163.com>
2022-08-08 00:49:02 +0800

    base-files: fix Segmentation fault (core dumped)

--- shell/scripts/fixtools.sh
+++ shell/scripts/fixtools.sh
@@ -9,3 +9,11 @@ NX_tools_python=$(whereis python | awk '{print $2}')
 ln -sf ${NX_tools_python} ./staging_dir/host/bin/python
 ln -sf ${NX_tools_python} ./staging_dir/host/bin/python3
 
+# https://github.com/openwrt/openwrt/issues/7299#issuecomment-1132793918
+# base-files: fix Segmentation fault (core dumped)
+# https://paste.debian.net/1241483
+
+make package/ucert/{clean,compile}
+
+find build_dir/{host*,toolchain-*} -name .built\* -exec touch {} \;
+touch staging_dir/{host*,toolchain-*,target-*}/stamp/.*
